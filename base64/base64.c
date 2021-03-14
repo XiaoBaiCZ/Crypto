9555b88a815142c7050cc752cf9fc68a934b0741
xiaobai <1063779828@qq.com>
2021-03-14 17:42:09 +0800

    调优base64结构

--- base64/base64.c
+++ base64/base64.c
@@ -31,29 +31,26 @@ const char base64_charset[64] = {
 
 //对应关系： b3  b2  b1  b0
 //            c2  c1  c0
-
 //base64单元
-typedef struct {
-    unsigned int b3 : 6;
-    unsigned int b2 : 6;
-    unsigned int b1 : 6;
-    unsigned int b0 : 6;
+typedef union {
+    struct {
+        unsigned int b3 : 6;
+        unsigned int b2 : 6;
+        unsigned int b1 : 6;
+        unsigned int b0 : 6;
+    } unit;
+    struct {
+        unsigned int c2 : 8;
+        unsigned int c1 : 8;
+        unsigned int c0 : 8;
+        unsigned int    : 8;
+    } src;
 } base64_unit;
-
-//原数据
-typedef struct {
-    unsigned char c2;
-    unsigned char c1;
-    unsigned char c0;
-} base64_src;
-
-//初始化base64_src
-void base64_init_b64src(base64_src *b_src, char *src);
-base64_unit *base64_from_b64src(base64_src *b_src);
 
 //初始化base64_unit
 void base64_init_unit(base64_unit *unit, char *src);
-base64_src *base64_from_b64unit(base64_unit *unit);
+//初始化base64_src
+void base64_init_src(base64_unit *unit, char *src);
 char base64_val2key(char val);
 
 int base64_encode(char **b64, char *src, int size) {
@@ -67,15 +64,14 @@ int base64_encode(char **b64, char *src, int size) {
     const int count = (size + fill) / BASE64_SRC_LEN;
     for (int i = 0; i < count; ++i) {
         //根据原数据，构建单个b64_src数据
-        base64_src b_src;
-        base64_init_b64src(&b_src, src + i * BASE64_SRC_LEN);
-        base64_unit *unit = base64_from_b64src(&b_src);
+        base64_unit unit;
+        base64_init_src(&unit, src + i * BASE64_SRC_LEN);
         //base64转换并赋值
         const int index = i * BASE64_UNIT_LEN;
-        (*b64)[index] = base64_charset[unit->b0];
-        (*b64)[index + 1] = base64_charset[unit->b1];
-        (*b64)[index + 2] = base64_charset[unit->b2];
-        (*b64)[index + 3] = base64_charset[unit->b3];
+        (*b64)[index] = base64_charset[unit.unit.b0];
+        (*b64)[index + 1] = base64_charset[unit.unit.b1];
+        (*b64)[index + 2] = base64_charset[unit.unit.b2];
+        (*b64)[index + 3] = base64_charset[unit.unit.b3];
     }
     //补位赋值=
     if (fill > 0) {
@@ -105,36 +101,27 @@ int base64_decode(char **src, char *b64, int size) {
         //根据base64单元数据
         base64_unit unit;
         base64_init_unit(&unit, b64 + i * BASE64_UNIT_LEN);
-        base64_src *b_src = base64_from_b64unit(&unit);
         //base64转换并赋值
         const int index = i * BASE64_SRC_LEN;
-        (*src)[index] = b_src->c0;
-        (*src)[index + 1] = b_src->c1;
-        (*src)[index + 2] = b_src->c2;
+        (*src)[index] = unit.src.c0;
+        (*src)[index + 1] = unit.src.c1;
+        (*src)[index + 2] = unit.src.c2;
     }
     (*src)[src_len - fill] = '\0';
     return src_len - fill;
 }
 
-void base64_init_b64src(base64_src *b_src, char *src) {
-    b_src->c0 = src[0];
-    b_src->c1 = src[1];
-    b_src->c2 = src[2];
-}
-
-base64_unit *base64_from_b64src(base64_src *b_src) {
-    return (base64_unit *)b_src;
+void base64_init_src(base64_unit *unit, char *src) {
+    unit->src.c0 = src[0];
+    unit->src.c1 = src[1];
+    unit->src.c2 = src[2];
 }
 
 void base64_init_unit(base64_unit *unit, char *src) {
-    unit->b0 = base64_val2key(src[0]);
-    unit->b1 = base64_val2key(src[1]);
-    unit->b2 = base64_val2key(src[2]);
-    unit->b3 = base64_val2key(src[3]);
-}
-
-base64_src *base64_from_b64unit(base64_unit *unit) {
-    return (base64_src *)unit;
+    unit->unit.b0 = base64_val2key(src[0]);
+    unit->unit.b1 = base64_val2key(src[1]);
+    unit->unit.b2 = base64_val2key(src[2]);
+    unit->unit.b3 = base64_val2key(src[3]);
 }
 
 char base64_val2key(char val) {
